4ed26504b0a5c06d26ce0ce40c663fce32b870cf
maaby200 <maaby200@gmail.com>
2021-08-19 14:55:41 +0200

    Summary for the custom exceptions.

--- GenericRPG/InvalidArmorException.cs
+++ GenericRPG/InvalidArmorException.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace GenericRPG
 {
+    /// <summary>
+    /// The exception that is thrown when the Armor attempted to be equipped
+    /// is not eligble. Either because of item level or armor type in relation to class.
+    /// </summary>
     public class InvalidArmorException : Exception
     {
         public InvalidArmorException(string message) : base(message)
--- GenericRPG/InvalidWeaponException.cs
+++ GenericRPG/InvalidWeaponException.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace GenericRPG
 {
+    /// <summary>
+    /// The exception that is thrown when the Weapon attempted to be equipped
+    /// is not eligble. Either because of item level or weapon type in relation to class.
+    /// </summary>
     public class InvalidWeaponException : Exception
     {
         public InvalidWeaponException(string message) : base(message)
